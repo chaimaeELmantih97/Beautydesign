380cb057120f9e9a13b9878afc83b0dca5080f6b
FeenTech <34220730+ayoubkarati1209@users.noreply.github.com>
2021-01-06 17:17:23 +0100

    breadcrumb color

--- resources/views/frontend/pages/blog-detail.blade.php
+++ resources/views/frontend/pages/blog-detail.blade.php
@@ -8,7 +8,7 @@
         <div class="container">
             <div class="row">
                 <div class="col col-xs-12">
-                    <h2>Article</h2>
+                    <h2 style="margin-top: 50px">Article</h2>
                     <ol class="breadcrumb">
                         <li><a href="{{route('home')}}">Accueil</a></li>
                         <li>Article</li>
